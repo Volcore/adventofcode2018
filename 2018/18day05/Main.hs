ed78348353aecf32141cb9916b9bf8a8300ba21c
Volker <volker@extality.com>
2018-12-05 14:49:00 +0100

    day5

--- 2018/18day05/Main.hs
+++ 2018/18day05/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.HUnit
+import Data.Char (toLower, toUpper)
 
 --------------------------------------------------------------------------------
 -- Day specific part. This is where the implementation happens
@@ -9,19 +10,43 @@ import Test.HUnit
 testInput = "dabAcCaCBAcCcaDA"
 
 tests = TestList [
-  addTest solveA testInput 10
-  -- addTest solveB testInput 4455
+  addTest solveA testInput 10,
+  addTest solveB testInput 4
   ]
-
 
 -- Parse the input
 parse :: String -> String
-parse = head. lines
+parse = head . lines
 
+-- Collapse if they are not identical but their lowercases are
+doesCollapse :: Char -> Char -> Bool
+doesCollapse x y = x /= y && toLower x == toLower y
+
+-- Collapses a sequence as much as possible and returns the result
+collapse :: String -> String
+collapse = foldr collapse' ""
+  where
+    collapse' x (y:xs)
+      | doesCollapse x y = xs
+    collapse' x xs = [x] ++ xs
 
 -- Solving function for part A
 solveA :: String -> Int
-solveA = length . parse
+solveA = length . collapse . parse
+
+-- Solving function for part B
+solveB :: String -> Int
+solveB = minimum
+        -- Apply the collapse function
+        . map (length . collapse)
+        -- remove the letter given by the first tuple element from the string
+        -- given in the second tuple element
+        . map (\(x,ys) -> [ y | y <- ys, not (y `elem` [x, toUpper x])])
+        -- create tuples of the input and each letter of the alphabet
+        . zip ['a'..'z']
+        -- create 26 copies of the input string in a list
+        . replicate 26
+        . parse
 
 --------------------------------------------------------------------------------
 -- Day-agnostic part. Is the same every day of AoC
@@ -39,5 +64,5 @@ main = do
   input <- readFile "input.txt"
   putStrLn "Solution for A:"
   print . solveA $ input
-  -- putStrLn "Solution for B:"
-  -- print . solveB $ input
+  putStrLn "Solution for B:"
+  print . solveB $ input
