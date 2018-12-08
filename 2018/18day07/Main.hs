8f832e5f69ec108193bba49c50024b648700c9a8
volker <volker@extality.com>
2018-12-08 17:04:18 +0100

    7b

--- 2018/18day07/Main.hs
+++ 2018/18day07/Main.hs
@@ -50,17 +50,45 @@ solveA = findPath . graph . parse
 
 -- finds the path from the graph structure for solution A
 simulate :: Int -> Int -> Map Char String -> Int
-simulate delay numw = step (0, replicate numw (0, ' '))
-  where step (t, w) m
-          | Map.null m = t
-          | otherwise = trace (show t ++ show w) step (t+nextt, w') m'
-          where nextt = 0 -- minimum . fst . unzip . w
-                m' = Map.empty
-                w' = trace (show w) w
-          -- where idlew
-          --         | 
-          --       w' = w
-          --       m' = m
+simulate delay numw = step (-1) (replicate numw (' ', 0))
+  where
+    step t w m
+        -- | trace ("step " ++ show t ++ ": " ++ show w ++ "  " ++ show m) False = undefined
+        | Map.null m = t + (maximum . snd . unzip $ w)
+        | otherwise =
+              -- this could be optimized by finding the largest timestep we can
+              -- do (minimum worker time), but it already works so it's fine
+          let deltat = 1
+              -- subtract the timesteps from each worker, but keep a minimum 0
+              ts = map (\(x, t) -> (x, max 0 (t - deltat))) $ w
+              -- Split workers into two groups, one ready (time = 0) and one
+              -- busy (time > 0)
+              (ready, busy) = List.partition ((== 0) . snd) $ ts
+              -- find all workers that have completed a task that now needs to
+              -- be removed from the dependency graph
+              rl = filter (/= ' ') . fst . unzip $ ready
+              -- Update the dependency graph, removing all completed tasks
+              readyM = foldl (\x y -> Map.map (\ys -> List.delete y ys) x) m rl
+              -- Run the assignment helper function on the updated list of
+              -- workers. this just passes in already busy workers, and only
+              -- runs the assignment function for every worker that is idle.
+              -- This was inspired by the reddit aoc2018 megathread.
+              (w', m') = foldl assign (busy, readyM) $ [1..length ready]
+          in step (t + deltat) w' m'
+          where
+            -- How long is a given task?
+            tasklen c = (fromEnum c) - (fromEnum 'A') + delay + 1
+            assign (cw, cm) _ 
+              -- | trace("assign " ++ show cw ++ " " ++ show cm) False = undefined
+              -- If nothing available, add worked back to front of list
+              | Map.null $ Map.filter (== []) cm = ((' ', 0) : cw ,cm)
+              -- Something is available for this worked
+              | otherwise =
+                -- find the next task by filtering the list for empty
+                -- dependencies and get the smallest/first element
+                let nextTask = fst . Map.findMin . Map.filter (== []) $ cm 
+                -- Add new worker to the front of the queue
+                in ((nextTask, tasklen nextTask) : cw , Map.delete nextTask cm)
 
 -- Solving function for part B
 solveB :: Int -> Int -> String -> Int
@@ -81,7 +109,6 @@ main = do
   tt <- runTestTT tests
   input <- readFile "input.txt"
   putStrLn "Solution for A:"
-  -- print . solveA $ input
+  print . solveA $ input
   putStrLn "Solution for B:"
-  -- print . (solveB 60 5) $ input
-  print . (solveB 0 2) $ testInput
+  print . (solveB 60 5) $ input
