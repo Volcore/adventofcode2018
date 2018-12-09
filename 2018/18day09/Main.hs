d9f860f27f5cd1db1130c7cd102658946c97098a
volker <volker@extality.com>
2018-12-09 12:38:58 +0100

    day 9

--- 2018/18day09/Main.hs
+++ 2018/18day09/Main.hs
@@ -3,13 +3,11 @@ module Main where
 import Test.HUnit
 import Data.Map (Map)
 import qualified Data.Sequence as Seq
--- import qualified Data.List as List
 import Debug.Trace
 
 --------------------------------------------------------------------------------
 -- Day specific part. This is where the implementation happens
 --------------------------------------------------------------------------------
-
 
 tests = TestList [
   addTest solveA (9, 25) 32,
@@ -24,19 +22,27 @@ tests = TestList [
 solveA :: (Int, Int) -> Int
 solveA (np, nm) = solve 0 (Seq.replicate np 0) (Seq.singleton 0) [1..nm]
   where 
-    replace s x n = Seq.update n x s
     solve idx scores marbles [] = maximum scores
     solve idx scores marbles (next:ms)
+      -- Don't enable this on all tests/input. It'll spam like crazy. Just use
+      -- it on the 9,25 input to get similar output to thats on the website
       -- | trace (show next ++ " " ++ show scores ++ " " ++ show marbles ++ " " ++ show idx) False = undefined
       | (next `mod` 23) == 0 = solve newIdx23 scores23 marbles23 ms
       | otherwise = solve newIdx scores marbles' ms
         where 
+          -- Current player's index
+          playerIdx = next `mod` np
+          -- The index after insertion
           newIdx = ((idx + 1) `mod` (length marbles) + 1)
-          playerIdx = (next `mod` np)
+          -- new list of marbles in the general case
           marbles' = Seq.insertAt newIdx next marbles
+          -- new index for te 23 case
           newIdx23 = ((idx + length marbles - 7) `mod` (length marbles))
+          -- new list of marbles for the 23 case
           marbles23 = Seq.deleteAt newIdx23 marbles
+          -- the additional score in the 23 case
           newScore23 = (marbles `Seq.index` newIdx23) + next
+          -- new list of scores for the 23 case
           scores23 = Seq.adjust' (newScore23 +) playerIdx scores
 
 --------------------------------------------------------------------------------
