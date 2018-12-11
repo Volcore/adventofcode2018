893f3364641c86cf150df9bfc5adecc3ff0152ad
volker <volker@extality.com>
2018-12-11 15:15:59 +0100

    day 10

--- 2018/18day10/Main.hs
+++ 2018/18day10/Main.hs
@@ -40,13 +40,39 @@ testInput = "position=< 9,  1> velocity=< 0,  2>\n\
             \position=<14,  7> velocity=<-2,  0>\n\
             \position=<-3,  6> velocity=< 2, -1>"
 
-tests = TestList [
-  addTest solveA testInput 4
-  ]
+parse :: String -> [[Int]]
+parse = map (map read)
+      . map (\x -> [x!!1, x!!2, x!!4, x!!5])
+      . map (Split.splitOneOf "<>,")
+      . lines
 
--- Solving function for part A
-solveA :: String -> Int
-solveA s = 0
+simulate :: Int -> [[Int]] -> [[Int]]
+simulate n = foldr (step') []
+  where
+    step' xs out = [xs!!0 + n * xs!!2, xs!!1 + n * xs!!3, xs!!2, xs!!3] : out
+
+bounds :: [[Int]] -> [Int]
+bounds state = [minimum xs, maximum xs, minimum ys, maximum ys]
+  where
+    xs = map (head) $ state
+    ys = map (head . drop 1) $ state
+
+atPosition :: Int -> Int -> [[Int]] -> Bool
+atPosition x y state = (length
+                       . filter (\(x':y':_) -> x' == x && y' == y) $ state) > 0
+
+plot :: [[Int]] -> IO ()
+plot state = do
+  let b = bounds $ state
+  putStrLn . (++) "Bounds: " . show $ b
+  putStr result
+  where
+      b = bounds $ state
+      contains x y
+        | atPosition x y state = '*'
+        | otherwise = ' '
+      row y = [contains x y | x <- [b!!0..b!!1]]
+      result = concat [row y ++ "\n" | y <- [b!!2..b!!3]]
 
 --------------------------------------------------------------------------------
 -- Day-agnostic part. Is the same every day of AoC
@@ -62,8 +88,8 @@ main :: IO ()
 main = do
   tt <- runTestTT tests
   input <- readFile "input.txt"
-  putStrLn "Solution for A:"
-  print . solveA $ input
-  putStrLn "Solution for B:"
-  print . solveA $ input
-  print . map (\x -> [x !! 1, x !! 2, x!!4, x!!5]) . map (Split.splitOneOf "<>,") . lines $ input
+  -- Solution for A & B
+  let steps = 10619
+  print $ steps
+  let state = simulate steps . parse $ input
+  plot $ state
