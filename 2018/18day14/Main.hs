8533488820118a34d80740dfceda29f23f6c9983
volker <volker@extality.com>
2018-12-13 22:12:01 -0800

    wip

--- 2018/18day14/Main.hs
+++ 2018/18day14/Main.hs
@@ -46,17 +46,10 @@ solveA n = map (\x -> toEnum (fromEnum '0' + x)) . take 10 . drop n . toList . f
 
 solveB :: [Int] -> Int
 solveB s = find s . toList . fst . runN 5000 $ initializer
--- solveB s = fst . foldl (up) (0, initializer) $ [1..]
---   where
---     up (n, (rs, es)) _
---       | trace (show rs) False = undefined
---       | s `isInfixOf` (toList rs) = (n, (rs, es))
---       | otherwise = (n+1, run $ (rs, es))
 
 find :: (Eq a, Show a) => [a] -> [a] -> Int
 find (x:xs) [] = -99999999
 find xs ys
-  -- | trace (show xs ++ " " ++ show ys) False = undefined
   | prefix xs ys = 0
   | otherwise = 1 + find xs (tail ys)
 
@@ -79,12 +72,6 @@ main :: IO ()
 main = do
   tt <- runTestTT tests
   putStrLn "Solution for A:"
-  -- print . solveA $ 306281
+  print . solveA $ 306281
   putStrLn "Solution for B:"
-  -- print . find [5,1,5,8,9] . toList . fst . runN 1000 $ initializer
-  print . find [3,0,6,2,8,1] . toList . fst . runN 10000000 $ initializer  
-  -- print . solveB $ input
-  -- print . run $ ([3, 7], [0, 1])
-  -- print . run . run $ ([3, 7], [0, 1])
-  -- print . run . run . run $ ([3, 7], [0, 1])
-  -- print . runN 9 $ ([3, 7], [0, 1])
+  print . find [3,0,6,2,8,1] . toList . fst . runN 25000000 $ initializer  
